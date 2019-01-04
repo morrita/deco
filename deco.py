7e32cd6115f22318f7d1d382591b76f6094a0fa0
Tom <tom_morris@binternet.com>
2019-01-04 17:35:39 +0000

    update via update_git.sh

--- deco.py
+++ deco.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python3
+#!/usr/bin/python
 # name: deco.py
 
 from toms_lib import get_date
@@ -9,12 +9,12 @@ tracelevel = 2
 
 
 @logging_decorator(tracelevel,logfile)
-def myfunc(myarg):
+def myfunc(myarg1, myarg2):
 
     datestr = get_date()
     update_file ("INFO: log entry made during function myfunc execution at: %s\n" % (datestr),logfile)
 
-    return "return value"
+    return "return values %s %s" % (myarg1, myarg2)
 
-r = myfunc('asdf')
+r = myfunc('asdf','qweqwr')
 print (r)
